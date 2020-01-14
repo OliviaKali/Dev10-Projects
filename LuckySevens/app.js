ec42b3ca961915260d58a499daab3f4f091ed74c
OKali <o.kalinowski27@gmail.com>
2020-01-14 17:11:20 -0500

    testing playGame function

--- LuckySevens/app.js
+++ LuckySevens/app.js
@@ -21,11 +21,37 @@ function playGame() {
         alert("Please enter starting bet greater than $0")
     }
     else {
+       while (bet > 0) {
         dice1 = rollDice();
         dice2 = rollDice();
         diceSum = dice1 + dice2;
         console.log(diceSum);
+       }
+        if (diceSum == 7) {
+            bet +=4
+            console.log("equal")
+        } 
+        else {
+            bet--
+            console.log("not equal") 
+            // if (bet > )
+        }
     }
+    document.getElementById("results").style.display ="block";
+    // document.getElementsById("playButton").innerText ="Play Again";
+    // document.getElementsById("startingBet").innerHTML = bet;
     return false;
 }
 
+// var startBet = document.getElementById('userStartingBet');
+// var bet;
+// var dice1;
+// var dice2;
+// var diceSum;
+// var totalRolls= 0;
+// var highestWon = 0;
+// var rollCountHighestWon = 0;
+
+// function playGame() {
+//     document.getElementById("results").style.display ="block";
+// }
