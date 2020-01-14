49cb6865cce5fa34ce89f67d13cea861ea5cbfa7
OKali <o.kalinowski27@gmail.com>
2020-01-14 17:59:49 -0500

    moved global vars to function to not have to reset the game

--- LuckySevens/app.js
+++ LuckySevens/app.js
@@ -2,21 +2,16 @@ function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
-//global variables
-var startBet;
-var bet;
-var dice1;
-var dice2;
-var diceSum;
-var totalRolls = 0;
-var highestWon = 0;
-var rollCountHighestWon = 0;
-
-function resetGame() {
-
-}
-
 function playGame() {
+    var startBet;
+    var bet;
+    var dice1;
+    var dice2;
+    var diceSum;
+    var totalRolls = 0;
+    var highestWon = 0;
+    var rollCountHighestWon = 0;
+
     startBet = document.getElementById("userStartingBet").value;
     bet = startBet
     console.log(bet)
@@ -29,9 +24,11 @@ function playGame() {
             dice1 = rollDice();
             dice2 = rollDice();
             diceSum = dice1 + dice2;
-            console.log(diceSum);
             totalRolls++;
+
+            console.log(diceSum);
             console.log(totalRolls);
+
             if (diceSum != 7) {
                 bet -= 1
             }
@@ -45,12 +42,11 @@ function playGame() {
         }
     }
 
-
     document.getElementById("results").style.display = "block";
-    document.getElementById("playButton").textContent ="Play Again";
+    document.getElementById("playButton").textContent = "Play Again";
     document.getElementById("startingBet").innerHTML = "$" + startBet + ".00";
     document.getElementById("totalRolls").innerHTML = totalRolls;
-    document.getElementById("highestMoneyWon").innerHTML = "$" + highestWon +".00"
+    document.getElementById("highestMoneyWon").innerHTML = "$" + highestWon + ".00"
     document.getElementById("rollCount").innerHTML = rollCountHighestWon
     return false;
 }
