ae1761305d6187bca29f17f90f35fe72291e1c5d
OKali <o.kalinowski27@gmail.com>
2020-01-14 14:48:21 -0500

    cleaning code and trying to connect js to html

---
+++ LuckySevens/app.js
@@ -0,0 +1,6 @@
+function playGame() {
+    // var startBet = Number(document.forms["luckySevensGame"]["startingBet"].value);
+    // console.log(startBet)
+    var x = document.forms["luckySevensGame"]["startingBet"].value;
+    alert(x);
+}
