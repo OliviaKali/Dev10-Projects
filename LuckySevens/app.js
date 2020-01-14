a3fef43dba6d4b818750511ebad42f5550dd3e61
OKali <o.kalinowski27@gmail.com>
2020-01-14 14:57:13 -0500

    added starting bet to appear in table when play is pressed

--- LuckySevens/app.js
+++ LuckySevens/app.js
@@ -1,6 +1,8 @@
 function playGame() {
     // var startBet = Number(document.forms["luckySevensGame"]["startingBet"].value);
     // console.log(startBet)
-    var x = document.forms["luckySevensGame"]["startingBet"].value;
-    alert(x);
+    var startBet = document.forms["luckySevensGame"]["userStartingBet"].value;
+    console.log(startBet);
+    document.getElementById("startingBet").innerText = startBet
 }
+
