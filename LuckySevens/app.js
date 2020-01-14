2c04d7ac1e29afdb862e55a63bb824aea53b4644
OKali <o.kalinowski27@gmail.com>
2020-01-14 17:30:46 -0500

    playGame function working and adding content from JS to HTML

--- LuckySevens/app.js
+++ LuckySevens/app.js
@@ -8,7 +8,7 @@ var bet;
 var dice1;
 var dice2;
 var diceSum;
-var totalRolls= 0;
+var totalRolls = 0;
 var highestWon = 0;
 var rollCountHighestWon = 0;
 
@@ -21,26 +21,28 @@ function playGame() {
         alert("Please enter starting bet greater than $0")
     }
     else {
-       while (bet > 0) {
-        dice1 = rollDice();
-        dice2 = rollDice();
-        diceSum = dice1 + dice2;
-        console.log(diceSum);
-        totalRolls++;
-        console.log(totalRolls);
-        if (diceSum != 7) {
-            bet -=1
-        } 
-        else {
-            bet +=4
+        while (bet > 0) {
+            dice1 = rollDice();
+            dice2 = rollDice();
+            diceSum = dice1 + dice2;
+            console.log(diceSum);
+            totalRolls++;
+            console.log(totalRolls);
+            if (diceSum != 7) {
+                bet -= 1
+            }
+            else {
+                bet += 4
+            }
         }
     }
-       }
-      
-      
-    document.getElementById("results").style.display ="block";
-    // document.getElementsById("playButton").innerText ="Play Again";
-    // document.getElementsById("startingBet").innerHTML = bet;
+
+
+    document.getElementById("results").style.display = "block";
+    document.getElementById("playButton").textContent ="Play Again";
+    document.getElementById("startingBet").innerHTML = "$" + startBet + ".00";
+    document.getElementById("totalRolls").innerHTML = totalRolls;
+    document.getElementById("highestMoneyWon").innerHTML = "$" +
     return false;
 }
 
