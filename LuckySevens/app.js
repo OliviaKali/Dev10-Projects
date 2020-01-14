d7b674d2354be1173ba93c224518f4b48df0df9e
OKali <o.kalinowski27@gmail.com>
2020-01-14 16:18:16 -0500

    remove comments

--- LuckySevens/app.js
+++ LuckySevens/app.js
@@ -1,30 +1,16 @@
-// document.getElementById("results").style.display = "none";
-
 function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
-// for (var i = 0; i < 100; i++) {
-//     console.log(rollDice());
-// }
 
 function playGame() {
     var startBet = document.getElementById("userStartingBet").value;
-    // var startBet = document.getElementById("userStartingBet");
-
-    // var startBet = document.forms["luckySevensGame"]["userStartingBet"].value;
+ 
     
     if (startBet > 0) {
         console.log(startBet);
     }
-    // alert(startBet);
-    // document.getElementById("startingBet").innerText = startBet;
-    // document.getElementById("totalRolls").innerText = startBet;
-    // document.getElementById("highestMoneyWon").innerText = startBet;
-    // document.getElementById("rollCount").innerText = startBet;
+
     return false;
 
-    // document.getElementById("results").innerText("<h1>Results</h1>")
-
-    // document.getElementById("results").style.display="block";
 }
 
